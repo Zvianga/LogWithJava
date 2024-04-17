eb5c8a8fa4cb6373560cd3bf7b79108f1c6777e5
GiorgiTurashvili <gturashvili735@gmail.com>
2024-04-18 02:18:46 +0400

    Add project files.

---
+++ LogWithJava/Logger.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+
+namespace LogWithJava
+{
+    public static class Logger
+    {
+        public static HashSet<Sinks> LoggedSinks = new HashSet<Sinks>();
+        public static Dictionary<Severity, string> FilePaths = new Dictionary<Severity, string>();
+
+        static Logger()
+        {
+            FilePaths[Severity.Info] = "..//InfoLog.txt";
+            FilePaths[Severity.Warning] = "..//WarningLog.txt";
+        }
+        public static void LogInformation(string message)
+        {
+            Log(Severity.Info, message);
+        }
+
+        public static void LogWarning(string message)
+        {
+            Log(Severity.Warning, message);
+        }
+        public static void LogDebug(string message, Severity severity = Severity.Info)
+        {
+#if DEBUG
+            Log(severity, message);
+#endif
+        }
+        private static void Log(Severity severity, string message)
+        {
+            if (message == null || message.Length == 0) throw new Exception("Invalid logging message. Log cannot be empty.");
+            foreach (var sink in LoggedSinks)
+            {
+                switch (sink)
+                {
+                    case Sinks.Console:
+                        Console.WriteLine($"{DateTime.UtcNow} [{severity.ToString().ToUpper()}] {message}");
+                        break;
+                    case Sinks.File:
+                        string path = FilePaths[severity];
+                        if (!File.Exists(path))
+                            File.Create(path).Close();
+                        var file = File.AppendText(path);
+                        file.WriteLine($"{DateTime.UtcNow} [{severity.ToString().ToUpper()}] {message}");
+                        file.Close();
+
+                        break;
+                    case Sinks.Debug:
+                        Debug.WriteLine($"{DateTime.UtcNow} [{severity.ToString().ToUpper()}] {message}");
+                        break;
+                }
+            }
+        }
+    }
+
+    public enum Sinks
+    {
+        Console,
+        File,
+        Debug
+    }
+
+    public enum Severity
+    {
+        Info,
+        Warning
+    }
+}
---
+++ LogWithJava/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LogWithJava
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.LoggedSinks = new HashSet<Sinks> { Sinks.Console, Sinks.File, Sinks.Debug };
+            Logger.LogInformation("Started logging");
+            Logger.LogWarning("Got warning that next is exception");
+            Logger.LogDebug("This will appear only if it's debugging");
+            Logger.LogDebug(null);
+        }
+    }
+}
